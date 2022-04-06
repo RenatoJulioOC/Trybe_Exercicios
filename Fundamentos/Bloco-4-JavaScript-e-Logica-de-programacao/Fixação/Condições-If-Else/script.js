f091953d698fa30b34978f8ac9b4d7a24adace70
Renato Julio <rnt.j.o.d.c@gmail.com>
2022-04-06 16:22:13 -0300

    Feat: Fixação

---
+++ Fundamentos/Bloco-4-JavaScript-e-Logica-de-programacao/Fixação/Condições-If-Else/script.js
@@ -0,0 +1,11 @@
+const resultadoDesafio = 85;
+
+if(resultadoDesafio >= 80) {
+    console.log("Parabéns, você foi aprovada(o).");
+}
+else if(resultadoDesafio< 80 && resultadoDesafio >= 60) {
+    console.log("Você está na nossa lista de espera.");
+}
+else {
+    console.log("Você foi reprovada(o).");
+}
