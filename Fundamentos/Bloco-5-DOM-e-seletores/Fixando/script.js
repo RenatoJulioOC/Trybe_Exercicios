9847aa44e5dc3b594cae798632a041e2d26835f6
Renato Julio <rnt.j.o.d.c@gmail.com>
2022-04-13 16:18:47 -0300

    Feat: Fixando

---
+++ Fundamentos/Bloco-5-DOM-e-seletores/Fixando/script.js
@@ -0,0 +1,13 @@
+//getElementById
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+
+console.log(document.getElementById("page-title").innerText = "O Poderoso Chefão");
+console.log(document.getElementById("second-paragraph").innerText = "Don Corleone");
+console.log(document.getElementById("subtitle").innerText = "Máfia Italiana"); 
+
+document.getElementsByClassName("para")[0];
+paragraph.style.color = "blue";
+
+const span = document.getElementsByTagName("span")[0];
+span.style.color = "green";
