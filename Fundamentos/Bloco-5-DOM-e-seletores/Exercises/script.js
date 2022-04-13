b8d2a60cf5fa4942c47b4b1f0a2f714387979834
Renato Julio <rnt.j.o.d.c@gmail.com>
2022-04-13 20:14:00 -0300

    Feat: Exercise 1

---
+++ Fundamentos/Bloco-5-DOM-e-seletores/Exercises/script.js
@@ -0,0 +1,26 @@
+/* //Exercise 1
+function changeText(){
+  let paragraph = document.getElementsByTagName('p')[1];
+  paragraph.innerHTML = "Salvador-BA, praia, água de côco, notebook na bag, e 1k pra brincar"
+}
+changeText();
+
+//Exercise 2
+function squareChangingGreen() {
+  let squareYellow = document.getElementsByClassName('main-content')[0];
+  squareYellow.style.background = "rgb(76,164,109)";
+}
+squareChangingGreen();
+
+//Exercise 3
+function squareChangingWhite() {
+  let squareRed = document.getElementsByClassName('center-content')[0];
+  squareRed.style.background = "white"
+}
+ *///squareChangingWhite();
+
+//Exercise 4
+function correctText() {
+  let text = document.getElementsByClassName('title')[0];
+  console.log(text)
+}
