5998fb6b5deb24a229d5bd25d8427dc599223ad1
Renato Julio <rnt.j.o.d.c@gmail.com>
2022-04-14 14:27:31 -0300

    Feat: ADM Trybe Time

---
+++ Fundamentos/Bloco-5-DOM-e-seletores/Fixando/Fixe/script.js
@@ -0,0 +1,41 @@
+ function addBackgroundHeader() {
+  let header = document.querySelector('header');
+  header.style.background = 'green';
+}
+addBackgroundHeader();
+
+function changeBackCH3() { 
+let h3 = document.querySelectorAll('.h3')[0];
+h3.style.background = "blueviolet"
+}
+changeBackCH3()
+
+function changeBackCH3z() { 
+  let h3z = document.querySelectorAll('.h3')[1];
+  h3z.style.background = "blueviolet"
+  }
+  changeBackCH3z()
+  
+function changeBackCNoEmerg() {
+  let yellow = document.querySelectorAll('.no-emergency-tasks')[0];
+  yellow.style.backgroundColor = "yellow"
+}
+changeBackCNoEmerg();
+
+function changeBackCEmerg(){
+  let salmon = document.querySelectorAll('.emergency-tasks')[0];
+salmon.style.backgroundColor = 'salmon'
+}
+changeBackCEmerg();
+
+function changeBackCNoEmergH3() {
+let black = document.querySelectorAll('.h3z')[0];
+black.style.background = "black"
+}
+changeBackCNoEmergH3();
+
+function changeBackCNoEmergH3s() {
+  let black = document.querySelectorAll('.h3z')[1];
+  black.style.background = "black"
+  }
+  changeBackCNoEmergH3s();
